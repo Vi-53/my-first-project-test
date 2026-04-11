fb5d06a60a2064104ffcd97ca736b45fa5982cf5
Vi-53 <alectosidorina@gmail.com>
2026-04-12 01:58:26 +0300

    Исправила код по замечаниям

--- test_1.py
+++ test_1.py
@@ -1,15 +1,17 @@
 from playwright.sync_api import Page, expect
-import random
-import string
+from faker import Faker
+
+fake = Faker()
+
+ADRESS = 'http://144.31.139.115:5000/.'
 
 def random_string(length=10):
     # тут создаю рандомные пароль и логин
-    line = string.ascii_letters + string.digits
-    return ''.join(random.choice(line) for _ in range(length))
+    return fake.password(length=length)
 
 def test_request(page: Page):
     # тут вхожу по ссылке на сайт и жду полного ответа
-    page.goto('http://144.31.139.115:5000/.', wait_until='load')
+    page.goto(ADRESS)
 
     # тут нахожусь на главной странице, кликаю на кнопку входа и жду загрузки нового адреса
     page.get_by_test_id('nav-login').click()
@@ -21,8 +23,8 @@ def test_request(page: Page):
     password = random_string()
 
     # тут их ввожу и кликаю на вход
-    page.locator('xpath=//input[@id="username"]').fill(username)
-    page.locator('xpath=//input[@id="password"]').fill(password)
+    page.locator('//*[@id="username"]').fill(username)
+    page.locator('//*[@id="password"]').fill(password)
     page.get_by_test_id('login-submit').click()
 
     # тут проверяю кружок загрузки
@@ -32,17 +34,5 @@ def test_request(page: Page):
 
     # тут проверяю надпись об ошибке
     error_text = page.get_by_test_id('login-error-inline')
-    expect(error_text).to_be_visible()
-    expect(error_text).to_have_text('Invalid login or password.')
-
-
-
-
-
-
-
-
-
-
-
-
+    assert error_text.is_visible(), "Элемент не появился:("
+    assert error_text.inner_text() == 'Invalid login or password.'
