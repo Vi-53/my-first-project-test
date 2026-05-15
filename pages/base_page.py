681b45b248c5f19c712f469f15908fa5b8925f7b
Vi-53 <alectosidorina@gmail.com>
2026-05-15 04:10:24 +0300

    Правки (удалила лишний мусор)

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,15 @@
 from playwright.sync_api import Page
 
+from config.settings import BASE_URL
+from ui.page_actions import PageActions
+
 
 class BasePage:
-    def __init__(self, page: Page):
+    PATH = ""
+
+    def __init__(self, page: Page) -> None:
         self.page = page
+        self.actions = PageActions(page)
+
+    def open(self) -> None:
+        self.actions.goto(f"{BASE_URL}{self.PATH}")
